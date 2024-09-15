80987d719fd4131f991d5de0be91748ec5753911
nomayen <nomayen.ohin@gmail.com>
2024-09-15 12:23:57 +0600

    first commit

---
+++ hello.C
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+    printf("Hello stack learner\n");
+    return 0;
+}
---
+++ print.C
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello New Bangladesh \n");
+    return 0;
+}
